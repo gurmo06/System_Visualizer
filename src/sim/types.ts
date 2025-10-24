9e9a65c33612b73797aad61e566aba55298dd648
gurmo06 <gurmo06@utexas.edu>
2025-10-23 23:02:13 -0500

    Created initial Instructions and CPUState interfaces.

---
+++ src/sim/types.ts
@@ -0,0 +1,17 @@
+export type Opcode = "ADD"
+
+export interface Instruction
+{
+    op: Opcode;         // Instruction opcode
+    rd?: number;            // Destination register
+    rn?: number;            // Source 1 register
+    rm?: number;            // Source 2 register
+    imm?: number;           // Immediate value
+}
+
+export interface CPUState
+{
+    regs: number[];    // Array of registers (X0 - X30) and XZR/SP (X31)
+    pc: number;             // Program counter
+    stall: boolean;         // Indicates if the CPU is stalled
+}
